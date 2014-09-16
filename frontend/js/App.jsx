0a538d4f0f5a05febec74706e57263881223cf51
Andrew Dunham <andrew@du.nham.ca>
2014-09-15 23:06:34 -0700

    Add TODOs

--- frontend/js/App.jsx
+++ frontend/js/App.jsx
@@ -7,6 +7,12 @@ var Route = RRouter.Route;
 var MainPage = require('./pages/MainPage.jsx');
 
 
+// TODO:
+//  - Set up immutable cursors for app state
+//  - Load Socket.IO and listen for changes
+//  - Pass state to current route
+
+
 var routes = (
     <Route>
         <Route path="/" view={MainPage} />
